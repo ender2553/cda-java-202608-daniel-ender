b2261ea47550dfc301d75a44f476f009449dbc84
Daniel Ender <danielender1982@yahoo.com>
2026-08-04 17:23:37 -0500

    variables and strings

--- cda-java/01-python-basics/exercises/01-variables-and-strings-in-python/exercise-variables-starter/variables-starter.py
+++ cda-java/01-python-basics/exercises/01-variables-and-strings-in-python/exercise-variables-starter/variables-starter.py
@@ -3,36 +3,41 @@
 # ## Exercise
 
 # 1. Create a variable named `onomatopoeia` and set its value to 'pop'.  Print the type for `onomatopoeia`.
-
-
+onomatopoeia="pop"
+print(type(onomatopoeia))
 
 # 2. Create a variable named `lyrics` and set its value to:
 #
-# ```
-# Katie Casey was baseball mad,
-# Had the fever and had it bad.
-# Just to root for the home town crew,
-# Ev'ry sou
-# Katie blew.
-# On a Saturday her young beau
-# Called to see if she'd like to go
-# To see a show, but Miss Kate said "No,
-# I'll tell you what you can do:"
-#
-# ```
+lyrics ="""
+Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"
+
+"""
 # Source: https://en.wikipedia.org/wiki/Take_Me_Out_to_the_Ball_Game - lyrics originally from 1908 Tin Pan Alley
 
 
 
 # 3. Create a variable named `gpa` for a grade point average and set it to 3.75.  Print out the variable.  Then print out the variable's type.
+gpa=3.75
+print(type(gpa))
+print(gpa)
 
 
 
 # 4. Create a variable called `empty_var` and set it to the Python equivalent for null.  Print out the variable.  Then print out the variable's type.
-
-
-
+empty_var = None
+print(type(empty_var))
+print(empty_var)
 # 5. Create a variable to store $8^{2}$. Print the variable.
+A1=828**282
+print(A1)
 
 
 
