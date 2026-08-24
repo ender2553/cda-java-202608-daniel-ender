92b3c947d88303790bb2f54b491d1de690b4b714
Daniel Ender <danielender1982@yahoo.com>
2026-08-24 16:28:53 -0500

    helloworldexcercise

---
+++ cda-java/04-java-language-fundamentals-secure-edition/code-along/HelloWorld/src/main/java/org/example/GameStats.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class GameStats {
+    public static void main(String[] args){
+
+    }
+
+    //Declare first, assign later (two separate statements)
+    int playerLevel;
+    playerLevel =1;
+        System.out.println("playerLevel: "+playerLevel);
+    //Declare first, assign later (two seperate statements)
+
+    int playerHealth = 100;
+        System.out.println("playerHealth: "+playerHealth);
+}
+
+    //BOOLEAN  (TRUE OR FALSE ONLY)
+
+    System.out.print(
